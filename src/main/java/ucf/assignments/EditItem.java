04097bc9d4ddc4ef408ba3e09544d0ee869105e9
MicahPB01 <micahpuccioball@gmail.com>
2021-07-09 12:08:41 -0400

    edit

--- src/main/java/ucf/assignments/EditItem.java
+++ src/main/java/ucf/assignments/EditItem.java
@@ -26,6 +26,16 @@ public class EditItem {
         //add string to the end of the txt file
         //close txt file
         //call showAll from loadItems class passing path to update table view
+        System.out.println("Entered EditItem.addItem");
+
+        Path filePath = Path.of(path);
+        try   {
+            Files.write((filePath), (properties[0] + "::" + properties[1] + "::" + properties[2] + "::Incomplete\n").getBytes(), StandardOpenOption.APPEND);
+        }
+        catch(Exception ignored)   {
+
+        }
+
 
 
         return "complete";
@@ -38,7 +48,42 @@ public class EditItem {
         //remove line
         //close txt file
         //call showAll from loadItems class passing path to update table view
+        System.out.printf("Entered EditItem.removeItem\n");
+        FileIO change = new FileIO();
+        String[] properties = new String[4];
+        String[] selectedProperties = new String[4];
+        String currentLine;
+        String newPath;
+        selectedProperties[0] = item.getName();
+        selectedProperties[1] = item.getDescription();
+        selectedProperties[2] = item.getDueDate();
+        selectedProperties[3] = item.getStatus();
+        System.out.printf("Name: %s\nDescription: %s\nDue Date: %s\nStatus: %s\n", selectedProperties[0],selectedProperties[1],selectedProperties[2],selectedProperties[3] );
+        ArrayList<String> newFileData = new ArrayList<>();
 
+        File file = new File(path);
+        try {
+            FileReader input = new FileReader(file);
+            BufferedReader bufferedReader = new BufferedReader(input);
+
+            currentLine = bufferedReader.readLine();
+            while(currentLine != null)   {
+                properties = currentLine.split("::");
+
+                ItemObject tempItem = new ItemObject(properties[0],properties[1],properties[2],properties[3]);
+
+                if(!matches(item, tempItem))   {
+                    newFileData.add(currentLine);
+                }
+                currentLine = bufferedReader.readLine();
+            }
+            bufferedReader.close();
+        }
+        catch(Exception ignored) {}
+        change.writeTempFile(newFileData);
+        newPath = change.createTempFile();
+        change.removeOldFile(path);
+        change.renameFile(path, newPath);
 
         return "complete";
     }
@@ -59,7 +104,44 @@ public class EditItem {
         //close txt file
         //call showAll from LoadItems class to update tableview
         System.out.printf("Entered EditItem.editDescription\n");
+        FileIO change = new FileIO();
+        String[] properties = new String[4];
+        String[] selectedProperties = new String[4];
+        String currentLine;
+        String newPath;
+        selectedProperties[0] = item.getName();
+        selectedProperties[1] = item.getDescription();
+        selectedProperties[2] = item.getDueDate();
+        selectedProperties[3] = item.getStatus();
+        ArrayList<String> newFileData = new ArrayList<>();
 
+        File file = new File(path);
+        try {
+            FileReader input = new FileReader(file);
+            BufferedReader bufferedReader = new BufferedReader(input);
+
+            currentLine = bufferedReader.readLine();
+            while(currentLine != null)   {
+                properties = currentLine.split("::");
+
+                ItemObject tempItem = new ItemObject(properties[0],properties[1],properties[2],properties[3]);
+
+                if(!matches(item, tempItem))   {
+                    newFileData.add(currentLine);
+                }
+                else   {
+                    currentLine = properties[0]+"::"+newDescription+"::"+properties[2]+"::"+properties[3];
+                    newFileData.add(currentLine);
+                }
+                currentLine = bufferedReader.readLine();
+            }
+            bufferedReader.close();
+        }
+        catch(Exception ignored) {}
+        change.writeTempFile(newFileData);
+        newPath = change.createTempFile();
+        change.removeOldFile(path);
+        change.renameFile(path, newPath);
 
         return "complete";
     }
@@ -73,7 +155,44 @@ public class EditItem {
         //close txt file
         //call showAll from LoadItems class to update tableview
         System.out.printf("Entered EditItem.editDescription\n");
+        FileIO change = new FileIO();
+        String[] properties = new String[4];
+        String[] selectedProperties = new String[4];
+        String currentLine;
+        String newPath;
+        selectedProperties[0] = item.getName();
+        selectedProperties[1] = item.getDescription();
+        selectedProperties[2] = item.getDueDate();
+        selectedProperties[3] = item.getStatus();
+        ArrayList<String> newFileData = new ArrayList<>();
 
+        File file = new File(path);
+        try {
+            FileReader input = new FileReader(file);
+            BufferedReader bufferedReader = new BufferedReader(input);
+
+            currentLine = bufferedReader.readLine();
+            while(currentLine != null)   {
+                properties = currentLine.split("::");
+
+                ItemObject tempItem = new ItemObject(properties[0],properties[1],properties[2],properties[3]);
+
+                if(!matches(item, tempItem))   {
+                    newFileData.add(currentLine);
+                }
+                else   {
+                    currentLine = properties[0]+"::"+properties[1]+"::"+newDate+"::"+properties[3];
+                    newFileData.add(currentLine);
+                }
+                currentLine = bufferedReader.readLine();
+            }
+            bufferedReader.close();
+        }
+        catch(Exception ignored) {}
+        change.writeTempFile(newFileData);
+        newPath = change.createTempFile();
+        change.removeOldFile(path);
+        change.renameFile(path, newPath);
 
 
         return "complete";
@@ -94,14 +213,88 @@ public class EditItem {
 
     public String markIncomeplete(String path, ItemObject item) throws IOException {
         System.out.printf("Entered EditItem.markComplete\n");
+        FileIO change = new FileIO();
+        String[] properties = new String[4];
+        String[] selectedProperties = new String[4];
+        String currentLine;
+        String newPath;
+        selectedProperties[0] = item.getName();
+        selectedProperties[1] = item.getDescription();
+        selectedProperties[2] = item.getDueDate();
+        selectedProperties[3] = item.getStatus();
+        ArrayList<String> newFileData = new ArrayList<>();
 
+        File file = new File(path);
+        try {
+            FileReader input = new FileReader(file);
+            BufferedReader bufferedReader = new BufferedReader(input);
+
+            currentLine = bufferedReader.readLine();
+            while(currentLine != null)   {
+                properties = currentLine.split("::");
+
+                ItemObject tempItem = new ItemObject(properties[0],properties[1],properties[2],properties[3]);
+
+                if(!matches(item, tempItem))   {
+                    newFileData.add(currentLine);
+                }
+                else   {
+                    currentLine = properties[0]+"::"+properties[1]+"::"+properties[2]+"::"+"Incomplete";
+                    newFileData.add(currentLine);
+                }
+                currentLine = bufferedReader.readLine();
+            }
+            bufferedReader.close();
+        }
+        catch(Exception ignored) {}
+        change.writeTempFile(newFileData);
+        newPath = change.createTempFile();
+        change.removeOldFile(path);
+        change.renameFile(path, newPath);
 
         return "complete";
     }
 
     public String markComplete(String path, ItemObject item) throws IOException {
         System.out.printf("Entered EditItem.markComplete\n");
+        FileIO change = new FileIO();
+        String[] properties = new String[4];
+        String[] selectedProperties = new String[4];
+        String currentLine;
+        String newPath;
+        selectedProperties[0] = item.getName();
+        selectedProperties[1] = item.getDescription();
+        selectedProperties[2] = item.getDueDate();
+        selectedProperties[3] = item.getStatus();
+        ArrayList<String> newFileData = new ArrayList<>();
 
+        File file = new File(path);
+        try {
+            FileReader input = new FileReader(file);
+            BufferedReader bufferedReader = new BufferedReader(input);
+
+            currentLine = bufferedReader.readLine();
+            while(currentLine != null)   {
+                properties = currentLine.split("::");
+
+                ItemObject tempItem = new ItemObject(properties[0],properties[1],properties[2],properties[3]);
+
+                if(!matches(item, tempItem))   {
+                    newFileData.add(currentLine);
+                }
+                else   {
+                    currentLine = properties[0]+"::"+properties[1]+"::"+properties[2]+"::"+"Complete";
+                    newFileData.add(currentLine);
+                }
+                currentLine = bufferedReader.readLine();
+            }
+            bufferedReader.close();
+        }
+        catch(Exception ignored) {}
+        change.writeTempFile(newFileData);
+        newPath = change.createTempFile();
+        change.removeOldFile(path);
+        change.renameFile(path, newPath);
 
         return "complete";
     }
